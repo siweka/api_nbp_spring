147e5669a555f2244fc937b19f4e8b24f5f63ed0
AS <jedrek.siwek@gmail.com>
2023-04-13 13:16:53 +0200

    names ok

--- src/main/java/com/isa/nbpapi_spring/NbpApiSpringApplication.java
+++ src/main/java/com/isa/nbpapi_spring/NbpApiSpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NbpapiSpringApplication {
+public class NbpApiSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NbpapiSpringApplication.class, args);
+        SpringApplication.run(NbpApiSpringApplication.class, args);
     }
 
 }
